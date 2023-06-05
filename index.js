b5fa8d4e73ba2c221277f360a386710308836c02
Alexey <dogadaev142@gmail.com>
2023-06-05 17:43:31 +0300

    Median of Two Sorted Arrays

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ var isPalindrome = function (x) {
   Number(reversed);
   return x == reversed ? true : false;
 };
+// 2.Median of Two Sorted Arrays
+var findMedianSortedArrays = function (nums1, nums2) {
+  let combinedNums = nums1.concat(nums2).sort((a, b) => a - b);
+  let midIdx = Math.floor(combinedNums.length / 2);
+  if (combinedNums.length % 2 === 0) {
+    return (combinedNums[midIdx] + combinedNums[midIdx - 1]) / 2;
+  } else {
+    return combinedNums[midIdx];
+  }
+};
