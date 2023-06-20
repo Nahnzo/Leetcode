7c93e1b32793ed35320e4cc1a6b293fe9a34e001
Alexey <dogadaev142@gmail.com>
2023-06-20 16:13:22 +0300

    add

--- index.js
+++ index.js
@@ -130,3 +130,14 @@ var once = function (fn) {
     }
   };
 };
+// 16.chunk
+var chunk = function (arr, size) {
+  const chunkedArray = [];
+  let index = 0;
+  while (index < arr.length) {
+    chunkedArray.push(arr.slice(index, index + size));
+    index += size;
+  }
+  return chunkedArray;
+};
+chunk([8, 5, 3, 2, 6], 2);
