2a56e7cbc3093fc4107b69c1df5f30f479979470
Alexey Dogadaev <89814318163@mail.ru>
2023-07-02 22:07:26 +0300

    add

--- index.js
+++ index.js
@@ -197,3 +197,17 @@ var mostWordsFound = function (sentences) {
   }
   return maxCount;
 };
+// 21.finalValueAfterOperations
+var finalValueAfterOperations = function (operations) {
+  let count = 0;
+  for (let i = 0; i < operations.length; i++) {
+    if (operations[i] === "X++") {
+      count++;
+    } else if (operations[i] === "++X") {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  return count;
+};
