86db5e7f10e20e7b2633d2e4b900bc2791631283
Dogadaev Alexey <dogadaev142@gmail.com>
2023-10-17 15:12:15 +0300

    Find the Index of the First Occurrence in a String

--- index.js
+++ index.js
@@ -330,3 +330,15 @@ var canJump = function (nums) {
   }
   return false;
 };
+
+var strStr = function (haystack, needle) {
+  if (needle === "") {
+    return 0;
+  }
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    if (haystack.substring(i, i + needle.length) === needle) {
+      return i;
+    }
+  }
+  return -1;
+};
